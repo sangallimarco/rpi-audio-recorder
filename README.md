b405505fd27831d1bff075f0b4e8506545381710
Marco Sangalli <sangalli.marco@gmail.com>
2020-06-27 00:35:59 +0200

    adding pipe to lame

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 change .env file
 pip3 install -r requirements.txt
+
+# requirements
+
+as root
+
+```
+pip3 install python-dotenv
+pip3 install dropbox
+```
--- recorder.py
+++ recorder.py
@@ -1,4 +1,4 @@
-import subprocess
+from subprocess import Popen, PIPE
 import os
 import time
 import signal
@@ -8,29 +8,31 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DURATION = '300'
+DURATION = '600'
 TOKEN = os.getenv("DROPBOX_TOKEN")
 DROPBOX_PATH = os.getenv("DROPBOX_PATH")
+AUDIO_CARD = os.getenv("AUDIO_CARD")
 
 def upload(resource, filename):
-        dbx = dropbox.Dropbox(TOKEN)
-        destinationfile = '%s/%s' % (DROPBOX_PATH, filename)
-        print(destinationfile)
-        with open(resource, 'rb') as f:
-                dbx.files_upload(f.read(), destinationfile)
-                print('uploaded')
+	dbx = dropbox.Dropbox(TOKEN)
+	destinationfile = '%s/%s' % (DROPBOX_PATH, filename)
+	print(destinationfile)
+	with open(resource, 'rb') as f:
+		dbx.files_upload(f.read(), destinationfile)
+		print('uploaded')
 
 def recordAudio(filename):
-        proc_args = ['arecord', '-D' , 'plughw:1,0' , '-c' , '1',  '-r' , '44100' , '-f', 'S32_LE' , '-t' , 'wav' , '-V' , 'mono' , '-v' , '-d', DURATION ,filename]
-        res = subprocess.call(proc_args)
-
+	arecord = Popen( ['arecord', '-f', 'S16_LE' ,'-c','1', '-r', '22050', '-t', 'raw', '-D', AUDIO_CARD, '-d', DURATION],  stdout=PIPE)
+	lame = Popen(['lame' ,'-r', '-s', '22.05', '-m', 'm', '-b', '64' ,'-', filename],  stdin=arecord.stdout, stdout=PIPE)
+	arecord.stdout.close()
+	output = lame.communicate()[0] 
 
 def main():
-        while True:
-                filename =  '%s.wav' % datetime.today().isoformat()
-                localfile = '/tmp/%s' % filename
-                recordAudio(localfile)
-                upload(localfile, filename)
-                os.remove(localfile)
+	while True:
+		filename = '%s.mp3' % datetime.today().isoformat()
+		localfile = '/tmp/%s' % filename
+		recordAudio(localfile)
+		upload(localfile, filename)
+		os.remove(localfile)
 
 main()
